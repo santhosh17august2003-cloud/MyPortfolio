4461858b677b6a4d2ab280da5aa76e0d85cbe5e0
santhosh17august2003-cloud <santhosh17august2003@gmail.com>
2025-10-28 19:28:56 +0530

    Upload new class

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,6 +17,8 @@ const Projects = () => (
       <div className='ChatGPT'>
         <h3>ChatGPT Like Application - python with Django </h3>
         <p>A responsive ChatGPT Like Application</p>
+        <p><b>Link:</b><a href="https://santhoshdocgbt.onrender.com">"https://santhoshdocgbt.onrender.com"</a></p>
+        <p><b>Github Link :</b> <a href="https://github.com/santhosh17august2003-cloud/ChatbotDocumentUpload.git">https://github.com/santhosh17august2003-cloud/ChatbotDocumentUpload.git</a></p>
       </div>
     </div>
   </section>
